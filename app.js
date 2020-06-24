5d13815309017ef13510e4bb62f3a7880155aa65
carolcol <caroline.nunes@itelios.com>
2020-06-23 22:25:54 -0300

    adding sql
    adding sql

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
-const express = require('express');
-const app = express();
-const bodyParser = require('body-parser');
 const path = require('path');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const errorController = require('./controllers/error');
+const db = require('./util/database');
+
+const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
@@ -9,12 +14,12 @@ app.set('views', 'views');
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
-
-
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
+
+app.use(errorController.get404);
 
 app.listen(3000);
